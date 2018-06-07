45a85fd363240c3c8623f6128b9c508be0dc2ead
prospero78su <prospero.78.su@gmail.com>
2018-06-07 13:06:52 +0200

    85 Добавление заготовки вспомогательного графического элемента "метка-список"

---
+++ пакАвтодор/пакГуи/пакМеткаСписок/модМеткаСписок.py
@@ -0,0 +1,17 @@
+# coding: utf8
+"""
+Модуль вспомогательного графического элемента "метка-список"
+"""
+if True:
+	from tkinter import Frame as клсРамка, Label as клсМетка, Entry as клсВвод
+
+class клсМеткаСписок(клсРамка):
+	def __init__(сам, предок, якорь, метка, значение):
+		сам.__якорь = якорь
+		клсРамка.__init__(сам, предок, border=3, relief="groove")
+		сам.метка = клсМетка(сам, text=метка, border=3, relief='ridge', width=20, justify='right')
+		сам.метка.pack(side='left', anchor='w')
+		сам.ввод = клсВвод(сам, border=3)
+		сам.ввод.insert(0, значение)
+		сам.ввод.pack(fill='x')
+		сам.pack(fill='x',side='top')
